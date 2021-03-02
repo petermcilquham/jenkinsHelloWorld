ef554ba1c6a94eeaf6c6f55aa038b7e668596800
peter <peter.lm.schmidt@gmail.com>
2021-03-02 14:27:05 +0100

    cleanup

--- src/main/java/com/example/demo/helloWorld/HomeController.java
+++ src/main/java/com/example/demo/helloWorld/HomeController.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    public String name= "anna";
-
     @GetMapping("/")
     public String home(){
-
         return "index";
-    }
-
-    public void doSomething(){
-            name = "anna changed";
     }
 
 }
